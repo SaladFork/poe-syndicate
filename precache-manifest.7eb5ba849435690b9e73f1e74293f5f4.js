a754b8bcba606e0f61511246362af1d004ca4ea8
Elad Shahar <SaladFork@gmail.com>
2019-09-18 17:33:36 -0400

    Updates

---
+++ precache-manifest.7eb5ba849435690b9e73f1e74293f5f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cd7a57693424ed006d4b07421727d0",
+    "url": "/poe-syndicate/index.html"
+  },
+  {
+    "revision": "79b16fa2d823c3da65df",
+    "url": "/poe-syndicate/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "981810311f5d83cd3c9e",
+    "url": "/poe-syndicate/static/js/2.5472b71c.chunk.js"
+  },
+  {
+    "revision": "79b16fa2d823c3da65df",
+    "url": "/poe-syndicate/static/js/main.e83af947.chunk.js"
+  },
+  {
+    "revision": "6eb50da2d75821efec13",
+    "url": "/poe-syndicate/static/js/runtime~main.0f35dc3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poe-syndicate/static/media/logo.5d5d9eef.svg"
+  }
+]);
